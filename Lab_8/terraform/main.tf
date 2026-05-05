3b62479864f63bf0a26bb28f8c4d66aac2b3867a
studynazar <gevakn@gmail.com>
2026-05-05 20:25:54 +0000

    Lab 8 CI/CD setup

--- Lab_8/terraform/main.tf
+++ Lab_8/terraform/main.tf
@@ -9,16 +9,18 @@ terraform {
 
 provider "docker" {}
 
-resource "docker_network" "lab8_net" {
+resource "docker_network" "lab8" {
   name = "lab8-network"
 }
 
 resource "docker_container" "app" {
   name  = "lab8-app"
   image = "nazarh88/prikm:latest"
+
   networks_advanced {
-    name = docker_network.lab8_net.name
+    name = docker_network.lab8.name
   }
+
   ports {
     internal = 80
     external = 8097
@@ -28,9 +30,11 @@ resource "docker_container" "app" {
 resource "docker_container" "node_exporter" {
   name  = "lab8-node-exporter"
   image = "prom/node-exporter:latest"
+
   networks_advanced {
-    name = docker_network.lab8_net.name
+    name = docker_network.lab8.name
   }
+
   ports {
     internal = 9100
     external = 9101
--- Lab_8/terraform/outputs.tf
+++ Lab_8/terraform/outputs.tf
@@ -1,11 +1,11 @@
-output "node_exporter_target" {
-  value = "lab8-node-exporter:9100"
-}
-
 output "app_url" {
   value = "http://localhost:8097"
 }
 
-output "ansible_inventory" {
+output "node_exporter_target" {
+  value = "lab8-node-exporter:9100"
+}
+
+output "inventory" {
   value = "[local]\nlocalhost ansible_connection=local\n"
 }
