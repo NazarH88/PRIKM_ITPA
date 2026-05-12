5c7c9b9273d9c745066e5f6e4f088de00e663a5e
NazarH <113088918+NazarH88@users.noreply.github.com>
2026-05-12 08:39:17 +0000

    Update main.tf

--- Lab_8/terraform/main.tf
+++ Lab_8/terraform/main.tf
@@ -9,25 +9,35 @@ terraform {
 
 provider "docker" {}
 
-resource "docker_image" "ubuntu" {
+# Використовуємо інше ім'я для образу, щоб скинути кеш
+resource "docker_image" "ubuntu_fresh" {
   name         = "ubuntu:20.04"
-  keep_locally = true
+  keep_locally = false
 }
 
-resource "docker_container" "app_node" {
+resource "docker_container" "app_server_new" {
   name  = "app_node"
-  image = docker_image.ubuntu.image_id
+  image = docker_image.ubuntu_fresh.image_id
+  
+  # Обов'язкові параметри, щоб контейнер не закривався
+  tty   = true
+  stdin_open = true
   command = ["tail", "-f", "/dev/null"]
+
   ports {
     internal = 80
     external = 8081
   }
 }
 
-resource "docker_container" "monitor_node" {
+resource "docker_container" "monitor_server_new" {
   name  = "monitor_node"
-  image = docker_image.ubuntu.image_id
+  image = docker_image.ubuntu_fresh.image_id
+  
+  tty   = true
+  stdin_open = true
   command = ["tail", "-f", "/dev/null"]
+
   ports {
     internal = 9090
     external = 9090
