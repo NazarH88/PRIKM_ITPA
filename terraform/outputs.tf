919766b48e02e86302c5a903cd6c57ce4f09b268
NazarH <113088918+NazarH88@users.noreply.github.com>
2026-05-12 09:38:35 +0000

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,15 +1,11 @@
 output "ansible_inventory" {
   value = <<EOT
-[all:vars]
-ansible_user=root
-ansible_password=root
-ansible_port=22
-ansible_become=yes
-
 [app_servers]
 app_node ansible_connection=docker
 
 [monitor_servers]
-monitor_node ansible_connection=docker
+prometheus ansible_connection=docker
+grafana ansible_connection=docker
+node_exporter ansible_connection=docker
 EOT
 }
