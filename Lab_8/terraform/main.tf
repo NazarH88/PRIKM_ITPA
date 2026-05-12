3af0007049a50045737c3a066543ebf34a92e287
NazarH <113088918+NazarH88@users.noreply.github.com>
2026-05-12 08:37:18 +0000

    Update main.tf

--- Lab_8/terraform/main.tf
+++ Lab_8/terraform/main.tf
@@ -9,18 +9,15 @@ terraform {
 
 provider "docker" {}
 
-resource "docker_image" "ubuntu_base" {
+resource "docker_image" "ubuntu" {
   name         = "ubuntu:20.04"
   keep_locally = true
 }
 
 resource "docker_container" "app_node" {
   name  = "app_node"
-  image = docker_image.ubuntu_base.image_id
-  # Змушуємо контейнер працювати постійно
+  image = docker_image.ubuntu.image_id
   command = ["tail", "-f", "/dev/null"]
-  must_run = true
-  
   ports {
     internal = 80
     external = 8081
@@ -29,16 +26,10 @@ resource "docker_container" "app_node" {
 
 resource "docker_container" "monitor_node" {
   name  = "monitor_node"
-  image = docker_image.ubuntu_base.image_id
+  image = docker_image.ubuntu.image_id
   command = ["tail", "-f", "/dev/null"]
-  must_run = true
-
   ports {
     internal = 9090
     external = 9090
-  }
-  ports {
-    internal = 3000
-    external = 3000
   }
 }
