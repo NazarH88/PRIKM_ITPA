46bc020198873fb45d7ef06a7f356942bac49122
NazarH <113088918+NazarH88@users.noreply.github.com>
2026-05-12 08:45:12 +0000

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,32 +9,30 @@ terraform {
 
 provider "docker" {}
 
-# Створюємо контейнери з ПРЯМИМ посиланням на образ
 resource "docker_container" "app_node_v2" {
-  name  = "app_node_new"
+  name  = "app_node"
   image = "ubuntu:20.04"
-  
-  # Обов'язково, щоб контейнер не "засинав"
+
   command = ["tail", "-f", "/dev/null"]
   tty = true
   stdin_open = true
 
   ports {
     internal = 80
-    external = 8085 # Змінив порт про всяк випадок
+    external = 8085
   }
 }
 
 resource "docker_container" "monitor_node_v2" {
-  name  = "monitor_node_new"
+  name  = "monitor_node"
   image = "ubuntu:20.04"
-  
+
   command = ["tail", "-f", "/dev/null"]
   tty = true
   stdin_open = true
 
   ports {
     internal = 9090
-    external = 9091 # Змінив порт
+    external = 9091
   }
 }
